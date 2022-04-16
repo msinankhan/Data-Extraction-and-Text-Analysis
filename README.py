fed1e55899417abbe42b06499c083f1f7f539a55
msinankhan <91521519+msinankhan@users.noreply.github.com>
2022-04-17 00:02:54 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,19 +1,20 @@
-# Data-Extraction-and-Text-Analysis
-"""This project is about extracting relevant textual data from website links and analyzing it across various parameters"""
+The Data-Extraction-and-Text-Analysis as the name suggests, is a text analysis project where I extract text from various links which are in an xlsx file.
+The relevant text is extracted from each of the link and is used for analysis across various parameters.
 
+The following are the parameters against which the text obtained is analyzed:
+  1.  POSITIVE SCORE
+  2.  NEGATIVE SCORE
+  3.  POLARITY SCORE
+  4.  SUBJECTIVITY SCORE
+  5.  AVG SENTENCE LENGTH
+  6.  PERCENTAGE OF COMPLEX WORDS
+  7.  FOG INDEX
+  8.  AVG NUMBER OF WORDS PER SENTENCE
+  9.  COMPLEX WORD COUNT
+  10. WORD COUNT
+  11. SYLLABLE PER WORD
+  12. PERSONAL PRONOUNS
+  13. AVG WORD LENGTH
 
-import pandas as pd
-import numpy as np
-import os
-import nltk
-import string
-import re
-import nltk
-from newspaper import Article
-from pathlib import Path
-from newspaper import Config
-from nltk.tokenize import word_tokenize
-from nltk.tokenize import sent_tokenize
-from nltk.corpus import stopwords
 
 
